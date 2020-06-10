8962adc113e4ee35e1d81b2203ad6d54a04adff5
Caroline <caroline_mendez@baylor.edu>
2020-06-09 20:15:49 -0500

    first notes

---
+++ implementing.py
@@ -0,0 +1,14 @@
+class myArray:
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def get(self, index):
+        return self.data[index]
+
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
